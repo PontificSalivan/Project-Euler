c2780af8eb2955712c82e21f0a67f5442a6cf11a
Arsen Manukyan <rasen13@mail.ru>
2019-11-15 13:58:45 +0300

    initial commit

---
+++ 17.py
@@ -0,0 +1,7 @@
+from num2words import num2words
+
+def number_count(num):
+    return sum(1 for l in num2words(num) if l.isalpha())
+print (sum(number_count(i) for i in range ( 1, 1001)))
+
+input()
