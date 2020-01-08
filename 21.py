04948530a8f91614131fc5f2dcb635448933ed25
Arsen Manukyan <rasen13@mail.ru>
2020-01-08 14:29:53 +0300

    21

--- 21.py
+++ 21.py
@@ -6,17 +6,16 @@ lst=list()
 for i in range (10,10001):
     if isPrime(i):
         continue
-    else:
-        for j in range (1,int(i/2)+1):
-            if int(i%j) == 0:
+    for j in range (1,int(i/2)+1):
+        if int(i%j) == 0:
+            lst.append(j)
+    if sum(map(int, lst)) != i:
+        secondBro = sum(map(int, lst))
+        lst=list()
+        for j in range (1, int(secondBro/2)+1):
+            if int(secondBro%j) ==0:
                 lst.append(j)
-        if sum(map(int, lst)) != i:
-            secondBro = sum(map(int, lst))
-            lst=list()
-            for j in range (1, int(secondBro/2)+1):
-                if int(secondBro%j) ==0:
-                    lst.append(j)
-            if sum(map(int, lst)) == i:
-                sumOfBros += secondBro + i
+        if sum(map(int, lst)) == i:
+            sumOfBros += secondBro + i
     lst=list()
 print(int(sumOfBros/2))
