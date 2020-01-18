3e0909158e747b64e6f35c3dde826c142678dc9b
Arsen Manukyan <rasen13@mail.ru>
2020-01-18 05:16:24 +0300

    37

--- 37.py
+++ 37.py
@@ -1,8 +1,10 @@
 def isPrime(n):
     return all(n % i for i in range(2, n))
 sumi=186 # first 4 numbers less than 100
-for i in range (100,1000001):
-
+count = 4
+for i in range (100,10**10):
+    if count == 11:
+        break
     if str(i)[0] == '3' or str(i)[0] == '7':
 
         if str(i)[len(str(i))-1] == '3' or str(i)[len(str(i))-1] == '7':
@@ -31,8 +33,9 @@ for i in range (100,1000001):
                             i = int(str(i)[::-1])
                             if isPrime(i)==False:
                                 break
-                            
+
                         if i == 0:
+                            count +=1
                             sumi += current_i      
 
 print(sumi)
