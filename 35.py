52dd5bf9caed86bff81e198a8706330ed30fdc90
Arsen Manukyan <rasen13@mail.ru>
2020-01-19 19:17:47 +0300

    35

--- 35.py
+++ 35.py
@@ -1,15 +1,18 @@
-def isPrime(n):
-    return all(n % i for i in range(2, n))
+from functions.prime import isPrime
 
-count= 4
-countC = 0
-for i in range (10,1000001):
-    if isPrime(i) and 
-        
-        print(i)
-    
-
-            
+count = 0
+for i in range (2,1000001):
+    if isPrime(i):
+        Gate = True
+        for _ in range (len(str(i))-1):
+            new_i = str(i)[len(str(i))-1] + str(i // 10)
+            if isPrime(new_i) == False:
+                Gate = False
+                break
+            i = int(new_i)
+        if Gate:
+            count += 1 
+              
 print(count)
 
 
