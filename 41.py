40357f35242eb60fa0999ab21bc040c76e4b8173
Arsen Manukyan <rasen13@mail.ru>
2020-01-23 02:23:46 +0300

    41

--- 41.py
+++ 41.py
@@ -1,21 +1,29 @@
 from functions.prime import isPrime
 
-pan_prime_max = 0
-for n in range (9876543, 9000000, -1):
+for n in range (9999999, 1000000, -2):
     if isPrime(n):
+        lst = list()
         str_n = str(n)
         count = 0
+
         for i in str_n:
             if i == '0':
                 break
-            if str_n.count(i) == 1:
+            elif str_n.count(i) == 1:
                 count += 1
             else:
                 break
-        if count == len(str_n):
-            pan_prime_max = n
-            print(pan_prime_max)
-print(pan_prime_max)
+
+        if count == len(str_n) and str_n.count('1')==1:
+            for i in str_n:
+                lst.append(int(i))
+            lst.sort()
+            if lst[6]-lst[5] == 1 and lst[5]-lst[4] == 1 and lst[4]-lst[3] == 1  and lst[3]-lst[2] == 1 and lst[2]-lst[1] == 1 and lst[1]-lst[0] == 1:
+                print(n)
+                exit(0)
+
+
+
 
     
     
