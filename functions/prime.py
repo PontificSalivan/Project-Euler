599310fdf5e1c6cd8d0a0dba7ca6a084009cd9a2
Arsen Manukyan <rasen13@mail.ru>
2020-05-03 20:57:47 +0300

    prime

--- functions/prime.py
+++ functions/prime.py
@@ -16,3 +16,13 @@ def isPrime(num, cache ={2: True}):
             return False
     cache[num] = True
     return True
+
+def prime_factors(n):
+    factors=[]
+    while n % 2 == 0:
+        factors.append(2)
+        n /= 2
+    for p in range(3, int(n)+1,2):
+        if n % p == 0 and isPrime(p):
+            factors.append(p)
+    return factors
