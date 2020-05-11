3483d8321c25059b59afa89b1c3353e083a4ef28
Arsen Manukyan <rasen13@mail.ru>
2020-05-11 21:34:06 +0400

    58

--- 51-100/58.py
+++ 51-100/58.py
@@ -1,12 +1,21 @@
 from functions.prime import isPrime
 
-diag_numbers = 2
-primes_count = 0
-count = 1
-for i in range(3,10,diag_numbers):
-    if isPrime(i):
-        primes_count += 1
-    count += 1
-print(primes_count/count)
+
+primes_count = 8
+count = 13
+len_of_square = 7
+
+while primes_count/count > 0.1:
+
+    for i in range(len_of_square**2,(len_of_square+2)**2+1,len_of_square+1):
+       
+        if isPrime(i):          
+            primes_count += 1
+    count += 4
+    len_of_square += 2
+
+print(len_of_square)
+    
+
 
 
