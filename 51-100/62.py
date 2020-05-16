ddaa75b2969afc3057999630cb6cc12b88c5e592
Arsen Manukyan <rasen13@mail.ru>
2020-05-16 11:11:51 +0400

    62

--- 51-100/62.py
+++ 51-100/62.py
@@ -1,6 +1,5 @@
 from itertools import permutations,groupby
 import sys
-from math import modf
 
 def value_of_number(i):
     value_of_0,value_of_1,value_of_2,value_of_3,value_of_4,value_of_5,value_of_6,value_of_7,value_of_8,value_of_9=0,0,0,0,0,0,0,0,0,0
@@ -27,11 +26,10 @@ def value_of_number(i):
             value_of_9 += 1
     return value_of_0,value_of_1,value_of_2,value_of_3,value_of_4,value_of_5,value_of_6,value_of_7,value_of_8,value_of_9
 
-
-
 duplicates = {}
 lst = list()
 new_lst=list()
+final_lst=list()
 
 for i in range(1,10000):
     lst.append(str(i**3))
@@ -47,9 +45,11 @@ for el, group in groupby(sorted(new_lst)):
         duplicates[el] = count 
 
 for i in duplicates:
-    for k in lst:
-        if value_of_number(i) == value_of_number(k) and len(i) == len(k):
-            print(k)
-            sys.exit()
-      
+    final_lst.append(i)
+final_lst.sort()   
 
+for k in lst:
+    if value_of_number(final_lst[0]) == value_of_number(k) and len(i) == len(k):
+        print(k)
+        sys.exit()
+      
